6c50f7cd5dab4ef7169717a65be7977c94670bb1
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-05-04 01:05:58 -0500

    Inicio de proyecto

---
+++ app/src/main/java/com/dev6am/todo/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dev6am.todo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dev6am.todo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
