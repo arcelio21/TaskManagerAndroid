8ec94bbb4561a851822eaaabbc8fba8b9b359289
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-05-05 10:13:47 -0500

    Clase que manejara lo que se haga en la actividad para crear tarea

---
+++ app/src/main/java/com/dev6am/todo/activity/AddTaskActivity.java
@@ -0,0 +1,16 @@
+package com.dev6am.todo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dev6am.todo.R;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+    }
+}
