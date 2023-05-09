25b473aeac0d744cca576c9fb9d5f79f170c15ae
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-05-09 18:19:43 -0500

    Se creo Clase que representa a Usuario, y otra que se usara para contener diferentes objetos

---
+++ app/src/main/java/com/dev6am/todo/model/Data.java
@@ -0,0 +1,13 @@
+package com.dev6am.todo.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@ToString
+public class Data {
+
+    private Object data;
+}
