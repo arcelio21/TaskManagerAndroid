9ab60dad694fa3fd87cae5af478ac8b25ec10347
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-05-09 23:24:13 -0500

    Se agrego metodo para manipular informacion de categorias

--- app/src/main/java/com/dev6am/todo/viewmodel/TaskViewModel.java
+++ app/src/main/java/com/dev6am/todo/viewmodel/TaskViewModel.java
@@ -1,19 +1,37 @@
 package com.dev6am.todo.viewmodel;
 
+import android.content.Context;
+
 import androidx.lifecycle.ViewModel;
 
+import com.dev6am.todo.model.Category;
+import com.dev6am.todo.model.Data;
 import com.dev6am.todo.model.Task;
+import com.dev6am.todo.repository.CategoryRepository;
+import com.dev6am.todo.util.GeneratorIdCategory;
+import com.dev6am.todo.util.ReaderWriterFIle;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.List;
 
 public class TaskViewModel extends ViewModel {
 
 
     private Task task;
-    private AtomicInteger generatorID;
+    private static final CategoryRepository categoryRepository= new CategoryRepository();
+
 
     public TaskViewModel() {
     }
 
 
+    public boolean addCategory(Category category, Context context){
+
+        return TaskViewModel.categoryRepository.addCategory(category,context);
+
+    }
+
+    public List<Category> getCategories(Context context){
+
+        return TaskViewModel.categoryRepository.getCategories(context);
+    }
 }
